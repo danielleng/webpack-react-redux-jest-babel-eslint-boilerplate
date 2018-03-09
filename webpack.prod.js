6e102650bdbaf4b9aba803439a7508646f687316
danlengjj <danlengjj@gmail.com>
2018-03-09 15:11:15 +0800

    Added production build step to Webpack for React

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
@@ -5,6 +6,9 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 module.exports = merge(common, {
   devtool: 'source-map',
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
     new UglifyJSPlugin({
       sourceMap: true
     })
