4af206ba4a7cba51cb77e678b12d7ace7c6c19c7
danlengjj <danlengjj@gmail.com>
2018-03-09 15:03:51 +0800

    Added React and Redux to build step, added sample component

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Import Javascript module
 import createMyModule from './myModule.jsx';
+
+// Import React component
+import MyComponent from "./MyComponent.jsx";
 
 const a = createMyModule();
 a.init();
 a.someBehaviour();
+
+ReactDOM.render(
+  <MyComponent />, 
+  document.getElementById("reactContainer")
+);
