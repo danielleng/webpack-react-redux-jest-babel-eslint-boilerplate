cfc49cd19053b8c33466cc7d2d809fe408e95f56
danlengjj <danlengjj@gmail.com>
2018-04-27 17:20:34 +0800

    Removed local normalize.css and added normalize v8 via NPM instead

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import "normalize.css";
+import 'normalize.css';
+import './css/index.css';
 
 // Import Javascript module
 import createMyModule from './myModule.jsx';
